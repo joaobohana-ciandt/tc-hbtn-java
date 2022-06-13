f6fc70e26dd89fe9bfed370b80492563b0957273
Joao Bohana <jbohana@ciandt.com>
2022-06-13 09:01:50 -0300

    fixed printed number

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,6 +1,6 @@
 public class PrintIntegerVariable {
     public static void main(String[] args){
-    	int numero = 374;
+    	int numero = 311;
 	System.out.println("Avenida Andaló, " + numero);
     }
 }
