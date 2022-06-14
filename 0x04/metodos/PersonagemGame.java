9e8df0af2f475cf35ed16602b8f868d9e4979108
Joao Bohana <jbohana@ciandt.com>
2022-06-14 10:19:49 -0300

    task metodos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,37 @@
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(saudeAtual - quantidadeDeDano < 0){
+            saudeAtual = 0;
+        } else {
+            saudeAtual -= quantidadeDeDano;
+        }
+    }
+
+    public void receberCura(int quantidadDeCura){
+        if(saudeAtual + quantidadDeCura > 100){
+            saudeAtual = 100;
+        } else {
+            saudeAtual += quantidadDeCura;
+        }
+    }
+
+}
