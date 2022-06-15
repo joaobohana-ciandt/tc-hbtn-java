c6ec5f98f8ab38042f2b8d060483d21a8a600d7e
Joao Bohana <jbohana@ciandt.com>
2022-06-15 08:55:22 -0300

    task agregacao

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,44 @@
+public class Empregado {
+    private int codigo;
+    private String nome;
+    private Endereco endereco;
+
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void apresentar() {
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Cidade: " + endereco.getCidade());
+        System.out.println("Pais: " + endereco.getPais());
+    }
+}
