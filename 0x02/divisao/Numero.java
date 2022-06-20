c4086b973bd6608cfd08f403d00569dc0e10b4aa
Joao Bohana <jbohana@ciandt.com>
2022-06-20 15:25:45 -0300

    task 00

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,12 @@
+public class Numero {
+    public static void dividir(int a, int b){
+        int res = 0;
+        try{
+            res = a / b;
+        } catch (ArithmeticException exception){
+            System.out.println("Nao eh possivel divir por zero.");
+        } finally {
+            System.out.println(a + " / " + b + " = " + res);
+        }
+    }
+}
