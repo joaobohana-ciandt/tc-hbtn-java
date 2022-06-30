c323684a013d826c3c8417cd2fae1806cc764708
Joao Bohana <jbohana@ciandt.com>
2022-06-30 09:46:00 -0300

    task00

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Blog {
+    private ArrayList<Post> posts;
+
+    public Blog() {
+        this.posts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post){
+        posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores(){
+        TreeSet<String> autores = new TreeSet<>();
+        for(Post p : posts){
+            autores.add(p.getAutor());
+        }
+
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+        TreeMap<String, Integer> categoriasContagem = new TreeMap<>();
+
+        for(Post p : posts){
+            if(categoriasContagem.containsKey(p.getCategoria())) {
+                Integer count = categoriasContagem.get(p.getCategoria());
+                count++;
+                categoriasContagem.put(p.getCategoria(), count);
+            } else {
+                categoriasContagem.put(p.getCategoria(), 1);
+            }
+        }
+
+        return categoriasContagem;
+    }
+}
