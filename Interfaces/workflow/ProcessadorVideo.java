02ede9d71db3512e8f6ad53b7bcb2ea2df3a4b8e
Joao Bohana <jbohana@ciandt.com>
2022-07-04 11:08:04 -0300

    task 00

---
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessadorVideo {
+    private List<CanalNotificacao> canais;
+
+    public ProcessadorVideo() {
+        this.canais = new ArrayList<>();
+    }
+
+    public void registrarCanal(CanalNotificacao canalNotificacao){
+        canais.add(canalNotificacao);
+    }
+
+    public void processar(Video video){
+        String mensagem = video.getArquivo() + " - " + video.getFormatoVideo();
+        Mensagem mensagemLog = new Mensagem(mensagem, TipoMensagem.LOG);
+
+        for(CanalNotificacao canal : canais){
+            canal.notificar(mensagemLog);
+        }
+    }
+}
