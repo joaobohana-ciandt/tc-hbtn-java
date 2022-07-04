3b739edf155ebaedc4fcec976e3022aaa2257ac0
Joao Bohana <jbohana@ciandt.com>
2022-07-04 16:15:25 -0300

    task01

---
+++ Interfaces_Exercicios/frete/provedores/Sedex.java
@@ -0,0 +1,20 @@
+package provedores;
+
+public class Sedex implements ProvedorFrete{
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+        double valorFrete = 0;
+        if(peso > 1000) {
+            valorFrete = valor * 0.1;
+        } else {
+            valorFrete = valor * 0.05;
+        }
+
+        return new Frete(valorFrete, TipoProvedorFrete.SEDEX);
+    }
+
+    @Override
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return TipoProvedorFrete.SEDEX;
+    }
+}
